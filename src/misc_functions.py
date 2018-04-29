04845a69b01cb6d8962aa63bff8596204b15b48b
ccampell <ccampell.asu@gmail.com>
2018-04-29 18:38:43 -0400

    Changed function parameter to more accurately reflect its usage by the callee.

--- src/misc_functions.py
+++ src/misc_functions.py
@@ -13,7 +13,7 @@ from torch.autograd import Variable
 from torchvision import models
 
 
-def convert_to_grayscale(cv2im):
+def convert_to_grayscale(gradient):
     """
         Converts 3d image to grayscale
 
@@ -23,7 +23,8 @@ def convert_to_grayscale(cv2im):
     returns:
         grayscale_im (numpy_arr): Grayscale image with shape (1,W,D)
     """
-    grayscale_im = np.sum(np.abs(cv2im), axis=0)
+    gradient = gradient.cpu().numpy()
+    grayscale_im = np.sum(np.abs(gradient), axis=0)
     im_max = np.percentile(grayscale_im, 99)
     im_min = np.min(grayscale_im)
     grayscale_im = (np.clip((grayscale_im - im_min) / (im_max - im_min), 0, 1))
