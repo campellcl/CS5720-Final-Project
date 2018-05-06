dcd544ef570d07ca641aa15f950a0d02c7790555
ccampell <ccampell.asu@gmail.com>
2018-05-06 01:13:24 -0400

    Getting good at bash. The file now walks every directory in results/PyTorchCNNVisualizations/. For each subdirectory (class) it creates a customized demuxer_input_class.txt file for use in animating all images in the subdirectory's plt/ dir.

--- src/animate.sh
+++ src/animate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-init ()
+generateDemuxerFiles ()
 {
-    SOURCE_DIR=../results/PyTorchCNNVisualizations/
+    SOURCE_DIR=../results/PyTorchCNNVisualizations
     CLASSES_TO_ANIM=($(find ../results/PyTorchCNNVisualizations -maxdepth 1 -type d -printf '%P\n'))
     for class in "${CLASSES_TO_ANIM[@]}"; do
         if [ -e "demuxer_input_$class.txt" ]; then
@@ -12,35 +12,50 @@ init ()
              DEMUXER_FILE_NAME="demuxer_input_$class.txt"
              touch "$DEMUXER_FILE_NAME"
         fi
-    done
-}
-init
-
-# Write demuxer_input:
-if [ -e "$DEMUXER_FILE_NAME" ]; then
-#    echo > demuxer_input.txt
-#    echo "line 1" > "$DEMUXER_FILE_NAME"
-#    rm "$DEMUXER_FILE_NAME"
-    let i=1
-
-    for dir in "${CLASSES_TO_ANIM[@]}"; do
         getRelPath()
         {
-            RELATIVE_PATH="$SOURCE_DIR$dir/plt"
+            RELATIVE_PATH="$SOURCE_DIR/$class/plt"
             FILE_PATHS=($(find "$RELATIVE_PATH" -maxdepth 1 -type f))
-            echo "RELATIVE PATH '$RELATIVE_PATH'" >> "$DEMUXER_FILE_NAME"
+#            echo "RELATIVE PATH '$RELATIVE_PATH'" >> "$DEMUXER_FILE_NAME"
 #            echo "FILE PATHS '$FILE_PATHS'" >> "$DEMUXER_FILE_NAME"
         }
         getRelPath
         for file in "${FILE_PATHS[@]}"; do
-            if [[ -f $file ]]; then
-                echo "file '$file'" >> "$DEMUXER_FILE_NAME"
-            fi
+            echo "file '$file'" >> "$DEMUXER_FILE_NAME"
+            echo "duration 1" >> "$DEMUXER_FILE_NAME"
+#            echo "FILE '$file'" >> "$DEMUXER_FILE_NAME"
         done
-#        echo "file '$SOURCE_DIR$dir/plt/'" >> "$DEMUXER_FILE_NAME"
+        echo "file '${FILE_PATHS[-1]}'" >> "$DEMUXER_FILE_NAME"
+        echo "duration 1" >> "$DEMUXER_FILE_NAME"
     done
+}
+generateDemuxerFiles
 
-    echo "line 2" >> "$DEMUXER_FILE_NAME"
-    printf "class: %d\n" 281 >> "$DEMUXER_FILE_NAME"
-fi
+## Write demuxer_input:
+#if [ -e "$DEMUXER_FILE_NAME" ]; then
+##    echo > demuxer_input.txt
+##    echo "line 1" > "$DEMUXER_FILE_NAME"
+##    rm "$DEMUXER_FILE_NAME"
+#    let i=1
+#
+#    for dir in "${CLASSES_TO_ANIM[@]}"; do
+#        getRelPath()
+#        {
+#            RELATIVE_PATH="$SOURCE_DIR$dir/plt"
+#            FILE_PATHS=($(find "$RELATIVE_PATH" -maxdepth 1 -type f))
+#            echo "RELATIVE PATH '$RELATIVE_PATH'" >> "$DEMUXER_FILE_NAME"
+##            echo "FILE PATHS '$FILE_PATHS'" >> "$DEMUXER_FILE_NAME"
+#        }
+#        getRelPath
+#        for file in "${FILE_PATHS[@]}"; do
+#            if [[ -f $file ]]; then
+#                echo "file '$file'" >> "$DEMUXER_FILE_NAME"
+#            fi
+#        done
+##        echo "file '$SOURCE_DIR$dir/plt/'" >> "$DEMUXER_FILE_NAME"
+#    done
+#
+#    echo "line 2" >> "$DEMUXER_FILE_NAME"
+#    printf "class: %d\n" 281 >> "$DEMUXER_FILE_NAME"
+#fi
 # sh animate_gif.sh C:\Users\ccamp\Documents\GitHub\CS5720-Final-Project\results\PyTorchCNNVisualizations\281 paramOutput.mp4 paramOutputGIF.gif
